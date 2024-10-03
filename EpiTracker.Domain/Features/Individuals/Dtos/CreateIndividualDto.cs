ea02d01dcfd65d1707848cb04beb5f16480a0a3e
00arc7 <00arc7@D-DS-SF-13189>
2024-10-03 17:07:18 -0400

    fix:CreateIndividualDto set for non immutability, for at run time object creation

--- EpiTracker.Domain/Features/Individuals/Dtos/CreateIndividualDto.cs
+++ EpiTracker.Domain/Features/Individuals/Dtos/CreateIndividualDto.cs
@@ -1,6 +1,23 @@
 ﻿namespace EpiTracker.Domain.Features.Individuals.Dtos;
 
-public record CreateIndividualDto(string Name, int Age, List<string> Symptoms, DateTime? DateDiagnosed)
+public sealed class CreateIndividualDto
 {
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<string> Symptoms { get; set; } = new();
+    public DateTime? DateDiagnosed { get; set; }
+
     public bool IsDiagnosed => DateDiagnosed.HasValue;
-};
+
+    public CreateIndividualDto(string name, int age, List<string> symptoms, DateTime? dateDiagnosed)
+    {
+        Name = name;
+        Age = age;
+        Symptoms = symptoms;
+        DateDiagnosed = dateDiagnosed;
+    }
+    public CreateIndividualDto()
+    {
+        
+    }
+}
